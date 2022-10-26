fb8e98069088e1c3560d56d0f527d72e20bef35f
unknown <pandeysandeep1190@gmail.com>
2022-10-26 19:47:04 +0530

    second

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,33 @@
 from flask import Flask, request
 
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import pickle
+
 app = Flask(__name__)
 
+recipe_df = pd.read_csv("df_recipes.csv")
 
-@app.route('/',methods=['GET'])
+model_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+model = hub.load(model_url)
+
+nnModel = pickle.load(open("rec.sav","rb"))
+
+
+
+@app.route('/',methods=['POST'])
 def index():
-    return "Hello, World!"
+
+    query = request.get_json()
+    print(query['query'])
+    
+    emb = model([str(query['query'])])
+    neighbors = nnModel.kneighbors(emb,return_distance=False)[0]
+    return str(recipe_df['recipe_name'].iloc[neighbors].tolist())
+
+
+
 
 
 if __name__ == "__main__":
